0ce17c68678270f4f088cde72383a70a3eda7175
Kumargaurvit <kumargaurvit@gmail.com>
2026-01-17 14:11:56 +0530

    Docker Setup

--- Dockerfile
+++ Dockerfile
@@ -8,11 +8,11 @@ COPY . /app
 WORKDIR /app
 
 # Install all requirements
-RUN RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
 
 # Expose port 8000 and 8501 for fastapi and streamlit
 EXPOSE 8000
 EXPOSE 8501
 
 # Run command to start project service
-CMD ["bash","-c","uvicorn src.api.predict_api:app --host 0.0.0.0 --port 8000 --reload & streamlit run churn_app.py"]
+CMD ["bash","-c","uvicorn src.api.predict_api:app --host 0.0.0.0 --port 8000 & streamlit run churn_app.py"]
--- churn_app.py
+++ churn_app.py
@@ -20,11 +20,11 @@ try:
     Gender = st.selectbox("Select your Gender", options=["Male","Female"], index=None)
     Age = st.slider("Select your Age", min_value=0, max_value=100, value=18)
     Tenure = st.slider("How many years you have been with the bank?", min_value=0, max_value=10, value=0)
-    Balance = st.number_input("How much money do you currently have in the bank account?", min_value=0, value=0.0, step=0.01, format="%.2f")
+    Balance = st.number_input("How much money do you currently have in the bank account?", min_value=0.0, value=0.0, step=0.01, format="%.2f")
     NumOfProducts = st.number_input("How many bank products you currently use?", min_value=0, max_value=10, value=0, step=1)
     HasCrCard = st.selectbox("Do you currently have a credit card (0->No, 1->Yes)?", options=[0,1], index=None)
     IsActiveMember = st.selectbox("Are you an active member of the bank (0->No, 1->Yes)?", options=[0,1], index=None)
-    EstimatedSalary = st.number_input("What is your estimated salary? (Monthly)", min_value=0, value=0.0, step=0.01, format="%.2f")
+    EstimatedSalary = st.number_input("What is your estimated salary? (Monthly)", min_value=0.0, value=0.0, step=0.01, format="%.2f")
 
     if st.button("Predict Customer Churn"):
         data = {
